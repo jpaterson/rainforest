88bf7f05bcbcb600e2e1e88f789f7e1b0be3fd49
Johnny <john@thebiggainer.com>
2013-11-11 19:59:53 -0500

    Adding flash alerts and notices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  <body>
+    <% if flash[:notice] %>
+      <p id="notice"><%= flash[:notice] %></p>
+    <% end %>
+    <% if flash[:alert] %>
+      <p id="alert"><%= flash[:alert] %></p>
+    <% end %>
+
+    <%= yield %>
+  </body>
+</html>
 end
